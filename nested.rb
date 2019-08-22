70706bd8d3f4ffe4f28a43971f46c3c2b8d37a08
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-22 22:24:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto]=>[:known_for]="Ruby",[:languages]=["LISP", "C"]
 programmer_hash[:alan_kay][:known_for] = "GUI"
     
 end
